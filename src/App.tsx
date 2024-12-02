5dcbd7d89d3bf4d0c9aa4ce3ecdcff9941720db6
nemanja <antonijevicnemanja68@gmail.com>
2024-12-02 14:00:08 +0100

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Ads from "./pages/Ads";
+import Home from "./pages/Home";
+import Ad from "./pages/id/Ad";
+import FavouriteAds from "./pages/id/FavouriteAds";
+import MyAds from "./pages/id/MyAds";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ads" element={<Ads />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/ad/:id" element={<Ad />} />
+        <Route path="/favourites/:id" element={<FavouriteAds />} />
+        <Route path="/myads/:id" element={<MyAds />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/id/FavouriteAds.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavouriteAds = () => {
+  return (
+    <div>FavouriteAds</div>
+  )
+}
+
+export default FavouriteAds

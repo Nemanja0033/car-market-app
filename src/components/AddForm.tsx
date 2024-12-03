bb639c81ccb2ab6af5710e6bfd57126d22d6f419
nemanja <antonijevicnemanja68@gmail.com>
2024-12-03 13:34:21 +0100

    add form responsivnes

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -34,138 +34,136 @@ const AddForm = () => {
     };
 
     return (
-        <div className="md:w-1/2 w-full bg-white md:mt-32 h-auto rounded-lg shadow-lg flex justify-center justify-self-center">
-            <div className="flex-row w-[80%] mt-6">
-                <div className="w-full">
-                    <form onSubmit={handleSubmit}>
+        <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+            <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <select
+                        value={brand}
+                        onChange={(e) => setBrand(e.target.value)}
+                        required
+                        className="w-full border shadow-lg rounded-lg h-10 px-3"
+                    >
+                        <option value="" disabled>
+                            Select Brand
+                        </option>
+                        {carBrands.map((brand) => (
+                            <option key={brand} value={brand}>
+                                {brand}
+                            </option>
+                        ))}
+                    </select>
+                    <input
+                        value={model}
+                        onChange={(e) => setModel(e.target.value)}
+                        className="w-full shadow-lg h-10 border rounded-lg px-3"
+                        type="text"
+                        placeholder="Model"
+                        required
+                    />
+                    <div className="flex flex-col md:flex-row md:gap-3 space-y-3 md:space-y-0">
                         <select
-                            value={brand}
-                            onChange={(e) => setBrand(e.target.value)}
+                            value={fuel}
+                            onChange={(e) => setFuel(e.target.value)}
                             required
-                            className="w-full border shadow-lg rounded-lg h-9"
+                            className="w-full border shadow-lg rounded-lg h-10 px-3"
                         >
                             <option value="" disabled>
-                                Select Brand
+                                Select Fuel Type
                             </option>
-                            {carBrands.map((brand) => (
-                                <option key={brand} value={brand}>
-                                    {brand}
+                            {fuelType.map((fuel) => (
+                                <option key={fuel} value={fuel}>
+                                    {fuel}
                                 </option>
                             ))}
                         </select>
                         <input
-                            value={model}
-                            onChange={(e) => setModel(e.target.value)}
-                            className="w-full shadow-lg h-9 border rounded-lg mt-3"
+                            className="w-full shadow-lg h-10 border rounded-lg px-3"
+                            placeholder="Engine displacement"
                             type="text"
-                            placeholder="Model"
+                            value={engineCm}
+                            onChange={(e) => setEngineCm(e.target.value)}
                             required
                         />
-                        <div className="flex w-full gap-3 items-center mt-3">
-                            <select
-                                value={fuel}
-                                onChange={(e) => setFuel(e.target.value)}
-                                required
-                                className="w-full border shadow-lg rounded-lg h-9"
-                            >
-                                <option value="" disabled>
-                                    Select Fuel Type
-                                </option>
-                                {fuelType.map((fuel) => (
-                                    <option key={fuel} value={fuel}>
-                                        {fuel}
-                                    </option>
-                                ))}
-                            </select>
-                            <input
-                                className="h-9 border shadow-lg rounded-lg"
-                                placeholder="Engine displacement"
-                                type="text"
-                                value={engineCm}
-                                onChange={(e) => setEngineCm(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="flex w-full items-center mt-3 gap-3">
-                            <select
-                                value={gearboxType}
-                                onChange={(e) => setGearbox(e.target.value)}
-                                required
-                                className="w-full border shadow-lg rounded-lg h-9"
-                            >
-                                <option value="" disabled>
-                                    Select Gearbox
-                                </option>
-                                {gearbox.map((gear) => (
-                                    <option key={gear} value={gear}>
-                                        {gear}
-                                    </option>
-                                ))}
-                            </select>
-                            <select
-                                value={color}
-                                onChange={(e) => setColor(e.target.value)}
-                                required
-                                className="w-full border shadow-lg rounded-lg h-9"
-                            >
-                                <option value="" disabled>
-                                    Select Color
-                                </option>
-                                {carColors.map((color) => (
-                                    <option key={color} value={color}>
-                                        {color}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className="flex w-full items-center mt-3 gap-3">
-                            <select
-                                value={carBodyType}
-                                onChange={(e) => setCarBodyType(e.target.value)}
-                                required
-                                className="w-full border shadow-lg rounded-lg h-9"
-                            >
-                                <option value="" disabled>
-                                    Select Body Type
-                                </option>
-                                {carBodyTypes.map((body) => (
-                                    <option key={body} value={body}>
-                                        {body}
-                                    </option>
-                                ))}
-                            </select>
-                            <select
-                                value={year}
-                                onChange={(e) => setYear(e.target.value)}
-                                required
-                                className="w-full border shadow-lg rounded-lg h-9"
-                            >
-                                <option value="" disabled>
-                                    Select Year
-                                </option>
-                                {carYears.map((year) => (
-                                    <option key={year} value={year}>
-                                        {year}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <input
-                            type="number"
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)}
-                            className="w-full shadow-lg h-9 border rounded-lg mt-3"
-                            placeholder="Price ($)"
+                    </div>
+                    <div className="flex flex-col md:flex-row md:gap-3 space-y-3 md:space-y-0">
+                        <select
+                            value={gearboxType}
+                            onChange={(e) => setGearbox(e.target.value)}
                             required
-                        />
-                        <button
-                            type="submit"
-                            className="mt-4 mb-8 w-full bg-primary text-white h-9 rounded-lg shadow-lg hover:bg-black"
+                            className="w-full border shadow-lg rounded-lg h-10 px-3"
                         >
-                            Submit
-                        </button>
-                    </form>
-                </div>
+                            <option value="" disabled>
+                                Select Gearbox
+                            </option>
+                            {gearbox.map((gear) => (
+                                <option key={gear} value={gear}>
+                                    {gear}
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            value={color}
+                            onChange={(e) => setColor(e.target.value)}
+                            required
+                            className="w-full border shadow-lg rounded-lg h-10 px-3"
+                        >
+                            <option value="" disabled>
+                                Select Color
+                            </option>
+                            {carColors.map((color) => (
+                                <option key={color} value={color}>
+                                    {color}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col md:flex-row md:gap-3 space-y-3 md:space-y-0">
+                        <select
+                            value={carBodyType}
+                            onChange={(e) => setCarBodyType(e.target.value)}
+                            required
+                            className="w-full border shadow-lg rounded-lg h-10 px-3"
+                        >
+                            <option value="" disabled>
+                                Select Body Type
+                            </option>
+                            {carBodyTypes.map((body) => (
+                                <option key={body} value={body}>
+                                    {body}
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            value={year}
+                            onChange={(e) => setYear(e.target.value)}
+                            required
+                            className="w-full border shadow-lg rounded-lg h-10 px-3"
+                        >
+                            <option value="" disabled>
+                                Select Year
+                            </option>
+                            {carYears.map((year) => (
+                                <option key={year} value={year}>
+                                    {year}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <input
+                        type="number"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        className="w-full shadow-lg h-10 border rounded-lg px-3"
+                        placeholder="Price ($)"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="w-full bg-primary text-white h-10 rounded-lg shadow-lg hover:bg-black"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         </div>
     );

c2b76c747ed2e2b11fc30793a9867831be07b47e
nemanja <antonijevicnemanja68@gmail.com>
2024-12-03 01:34:48 +0100

    wrong credentials alert

--- src/actions/Login.ts
+++ src/actions/Login.ts
@@ -1,48 +1,56 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
-import { auth, provider } from '../../config/firebase.ts'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../config/firebase.ts';
 import { createUser } from './CreateUser.ts';
-
 
 //this function provide user login, storing is auth in local storage, and call function that create user in db
 
 //google method
 export const signUpWithGoogle = async (setIsAuth: Function, setUserName: Function) => {
-    signInWithPopup(auth, provider).then(async (result) => {
-        setIsAuth(true);
-        setUserName(result.user.displayName || "");
-        localStorage.setItem("isAuth", "true");
-        localStorage.setItem("userName", result.user.displayName || "");
-        localStorage.setItem("userID", result.user.uid)
-        createUser();
-        location.href = '/';
-    })
-}
+    signInWithPopup(auth, provider)
+        .then(async (result) => {
+            setIsAuth(true);
+            setUserName(result.user.displayName || "");
+            localStorage.setItem("isAuth", "true");
+            localStorage.setItem("userName", result.user.displayName || "");
+            localStorage.setItem("userID", result.user.uid);
+            createUser();
+            location.href = '/';
+        })
+        .catch((error) => {
+            alert('Error signing in with Google: ' + error.message);
+        });
+};
 
 //email and pass method
-export const signUpWithEmail =  async (setIsAuth: Function, setUserName: Function, email: string, password: string) => {
-    createUserWithEmailAndPassword(auth, email, password).then(async (result) => {
-        console.log(result.user.email)
-        setIsAuth(true);
-        setUserName(result.user.displayName || "");
-        localStorage.setItem("isAuth", "true");
-        localStorage.setItem("userName", result.user.displayName || "");
-        localStorage.setItem("userID", result.user.uid)
-        createUser();
-        location.href = '/';
-
-    })
-}
+export const signUpWithEmail = async (setIsAuth: Function, setUserName: Function, email: string, password: string) => {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(async (result) => {
+            setIsAuth(true);
+            setUserName(result.user.displayName || "");
+            localStorage.setItem("isAuth", "true");
+            localStorage.setItem("userName", result.user.displayName || "");
+            localStorage.setItem("userID", result.user.uid);
+            createUser();
+            location.href = '/';
+        })
+        .catch((error) => {
+            alert('Error signing up: ' + error.message);
+        });
+};
 
 // sign in with email
-export const loginWithEmail =  async (setIsAuth: Function, setUserName: Function, email: string, password: string) => {
-    signInWithEmailAndPassword(auth, email, password).then(async (result) => {
-        setIsAuth(true);
-        setUserName(result.user.displayName || "");
-        localStorage.setItem("isAuth", "true");
-        localStorage.setItem("userName", result.user.displayName || "");
-        localStorage.setItem("userID", result.user.uid)
-        createUser();
-        location.href = '/';
-
-    })
-}
+export const loginWithEmail = async (setIsAuth: Function, setUserName: Function, email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then(async (result) => {
+            setIsAuth(true);
+            setUserName(result.user.displayName || "");
+            localStorage.setItem("isAuth", "true");
+            localStorage.setItem("userName", result.user.displayName || "");
+            localStorage.setItem("userID", result.user.uid);
+            createUser();
+            location.href = '/';
+        })
+        .catch((error) => {
+            alert('Error logging in: ' + error.message);
+        });
+};
